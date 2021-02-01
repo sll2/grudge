78f92dfca98873d2ed2d26ff13db8848c331339f
Shelby Lockhart <sll2@illinois.edu>
2021-02-01 15:56:29 -0600

    updating variables

--- grudge/eager.py
+++ grudge/eager.py
@@ -399,7 +399,7 @@ class _RankBoundaryCommunication:
         local_data = flatten(self.local_dof_array)
 
         # Need size of array after flattened -- could put this code somewhere else
-        group_sizes = [grp_ary.shape[0] * grp_ary.shape[1] for grp_ary in ary]
+        group_sizes = [grp_ary.shape[0] * grp_ary.shape[1] for grp_ary in self.local_dof_array]
         group_starts = np.cumsum([0] + group_sizes)
         local_data_size = group_starts[-1]
 
